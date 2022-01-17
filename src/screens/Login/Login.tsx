f0a730382f3c1ad13afcec069430da6c48494ca5
王珠峰 <734765725@qq.com>
2022-01-17 09:10:18 +0800

    Fragment

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <>
+    <Fragment key={3}>
       登录页面
-    </ >
+    </Fragment >
   )
 }
--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <>
+    <Fragment ket={2}>
       注册页面
-    </>
+    </Fragment>
   )
 }
